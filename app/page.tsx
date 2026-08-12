461ee85d04725668e6f051926f1f921528751ed8
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-08-12 02:15:08 -0700

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 // import Image from "next/image";
+import styles from "./page.module.css";
+
 
 export default function Home() {
   return (
-    <>
-      <h1>To-Do List</h1>
-    </>
-    
-  
+    <div className={styles.page}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Your Tasks</h1>
+        <p className={styles.subtitle}>
+          Manage your tasks effectively with our beautiful board.
+        </p>
+      </div>
+    </div>
   );
 }
